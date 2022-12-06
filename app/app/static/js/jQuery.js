630b119b8c2c89c06fa01fcc84314623588a249b
Jason Pu <jason.pu19@outlook.com>
2022-12-06 18:55:29 -0500

    我直接搬到views.py里了

--- app/app/static/js/jQuery.js
+++ app/app/static/js/jQuery.js
@@ -10879,3 +10879,9 @@ if ( typeof noGlobal === "undefined" ) {
 
 return jQuery;
 } );
+
+const group = await getGroupInternal(groupID);
+let isAdmin = isGroupAdminByGroup(group, userID);
+if (!isAdmin) {
+	return httpCodes.Forbidden();
+}
--- app/app/views.py
+++ app/app/views.py
@@ -8,7 +8,12 @@ from django.http import HttpResponseNotFound, HttpResponse, HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 from django.views.decorators.csrf import csrf_exempt
 
-from .settings import MEDIA_ROOT
+
+import cv2
+import torch
+import albumentations as A
+
+from model import *
 
 def index(request):
     context = {}
@@ -18,4 +23,53 @@ def index(request):
 def detect(request):
     data = request.POST.getlist('data[]')
     print(data)
+    
     return JsonResponse({"result": "Smiling"})
+
+
+# Global variable to check if a model is used for the first time
+MODELWARMUP = True
+
+# Display image from path
+def display_image(image_path):
+    from PIL import Image
+    return Image.open(image_path)
+
+# Read an image to array
+def get_image_arr(image_pth):
+    return cv2.imread(image_pth)
+
+# Transform an image to fit the inference model
+def image_transform(image, transform):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return transform(image=image)['image']
+
+# Load the inference model from path
+def load_model(model_path, device):
+    # Load model
+    inference_model = torch.load(model_path)
+    inference_model.eval()
+    inference_model.to(device)
+
+    # Warm up
+    global MODELWARMUP
+    if MODELWARMUP:
+        inference_model(torch.randn(1, 1, 64, 64).to(device))
+        MODELWARMUP = False
+    return inference_model
+
+
+def inference(image_arr, model_path, device='cpu'):
+    # Load image from path
+    transform = A.Compose([
+        A.Resize(64, 64),
+        A.Normalize(0.449, 0.226)
+    ])
+    image = image_transform(image_arr, transform)
+    image = torch.tensor(image).unsqueeze(0).unsqueeze(0).to(device)
+
+    # Load the inference model
+    inference_model = load_model(model_path, device)
+
+    # Get outputs and normalize
+    return torch.sigmoid(inference_model(image))[0].tolist()
